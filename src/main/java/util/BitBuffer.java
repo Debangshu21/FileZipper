b52bed7ab33388e2ee9ad905176098a6e921c1e9
Debangshu21 <debangshu7772@gmail.com>
2024-09-06 12:20:42 +0530

    Updated BufferBit.java

--- src/main/java/util/BitBuffer.java
+++ src/main/java/util/BitBuffer.java
@@ -1,8 +1,9 @@
-
 package main.java.util;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 
 public class BitBuffer implements AutoCloseable {
     private int buffer;
@@ -41,9 +42,43 @@ public class BitBuffer implements AutoCloseable {
         out.flush();
     }
 
+    public int readBit(InputStream in) throws IOException {
+        if (bitsInBuffer == 0) {
+            int byteRead = in.read();
+            if (byteRead == -1) return -1;  // End of stream
+            buffer = byteRead;
+            bitsInBuffer = 8;
+        }
+        int bit = (buffer >> (bitsInBuffer - 1)) & 1;
+        bitsInBuffer--;
+        if (bitsInBuffer == 0) {
+            buffer = 0;  // Clear buffer after reading
+        }
+        return bit;
+    }
+
+    public byte[] toByteArray() {
+        int byteLength = (bitsInBuffer + 7) / 8;
+        ByteBuffer byteBuffer = ByteBuffer.allocate(byteLength);
+        for (int i = 0; i < byteLength; i++) {
+            int byteToWrite = buffer >>> (8 * (byteLength - i - 1));
+            byteBuffer.put((byte) (byteToWrite & 0xFF));
+        }
+        return byteBuffer.array();
+    }
+
+    public static BitBuffer fromByteArray(byte[] byteArray) {
+        BitBuffer bitBuffer = new BitBuffer();
+        int buffer = 0;
+        for (byte b : byteArray) {
+            buffer = (buffer << 8) | (b & 0xFF);
+        }
+        bitBuffer.buffer = buffer;
+        bitBuffer.bitsInBuffer = byteArray.length * 8;
+        return bitBuffer;
+    }
+
     private void resetBuffer() {
-        // This method is called internally when the buffer is full
-        // It doesn't actually write to an output stream, just resets the buffer
         buffer = 0;
         bitsInBuffer = 0;
     }
